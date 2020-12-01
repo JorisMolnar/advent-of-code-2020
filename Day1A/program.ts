07fdb62e61b08d5f5b61cf5a74d482cdb601ed00
Joris Molnar <joris.molnar@gmail.com>
2020-12-01 21:20:53 +0100

    Add day 1A

---
+++ Day1A/program.ts
@@ -0,0 +1,38 @@
+import { performance } from "perf_hooks";
+
+export class Program {
+  main(input: string): void {
+
+    const t0 = performance.now();
+    const result = this.calcResult(input);
+    const t1 = performance.now();
+
+    console.log(`Calc took ${t1 - t0} milliseconds.`);
+    console.log("Answer:", result);
+  }
+
+  private calcResult(input: string): number {
+    const numbers = this.parseInput(input);
+
+    const [n1, n2] = this.findCombination(numbers);
+
+    console.log(n1, n2);
+    return n1 * n2;
+  }
+
+  private findCombination(numbers: number[]): [number, number] {
+    for (const n1 of numbers) {
+      for (const n2 of numbers) {
+        if (n1 + n2 === 2020) {
+          return [n1, n2];
+        }
+      }
+    }
+
+    throw new Error("No combination found");
+  }
+
+  private parseInput(input: string): number[] {
+    return input.split("\n").map(s => Number(s));
+  }
+}
