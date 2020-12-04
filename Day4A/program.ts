4bb1c4922e95683d270fe15b6a37ebe0c18a870e
Joris Molnar <joris.molnar@gmail.com>
2020-12-04 08:28:33 +0100

    Add day 4A

---
+++ Day4A/program.ts
@@ -0,0 +1,39 @@
+import { performance } from "perf_hooks";
+
+const requiredFields: string[] = [
+  "byr",
+  "iyr",
+  "eyr",
+  "hgt",
+  "hcl",
+  "ecl",
+  "pid",
+  // "cid"
+];
+
+export class Program {
+  main(input: string): void {
+    const t0 = performance.now();
+    const result = this.calcResult(input);
+    const t1 = performance.now();
+
+    console.log(`Calc took ${t1 - t0} milliseconds.`);
+    console.log("Answer:", result);
+  }
+
+  private calcResult(input: string): number {
+    const passwords = this.parseInput(input);
+
+    const valid = passwords.filter(this.checkValid);
+
+    return valid.length;
+  }
+
+  private checkValid(passport: string): boolean {
+    return requiredFields.every(req => passport.includes(req));
+  }
+
+  private parseInput(input: string): string[] {
+    return input.split("\n\n");
+  }
+}
